931b7084ffdb2e0383e3dddf25443df29c45e111
TGDev2 <garintheo76@gmail.com>
2024-12-05 11:44:37 +0100

    Deploiment

--- LinkUpAPI/Program.cs
+++ LinkUpAPI/Program.cs
@@ -9,6 +9,12 @@ using LinkUpAPI.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
+
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
